921e965f9964041c47542ab711e2152d3fd055b5
sjparker94 <simon.parker1@homegroup.org.uk>
2024-10-23 13:20:15 +0100

    feat: make variable to share prettier settings & add bash and python formatting

--- .config/nvim/lua/sjparker94/plugins/formatting.lua
+++ .config/nvim/lua/sjparker94/plugins/formatting.lua
@@ -5,21 +5,33 @@ return {
 	config = function()
 		local conform = require("conform")
 
+		local prettier_settings = { "prettierd", "prettier", stop_after_first = true }
+
 		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				scss = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
+				go = { "goimports", "gofumpt", "goimports-reviser" },
+				bash = { "shfmt" },
+				sh = { "shfmt" },
+				fish = { "fish_indent" },
+				javascript = prettier_settings,
+				typescript = prettier_settings,
+				javascriptreact = prettier_settings,
+				typescriptreact = prettier_settings,
+				svelte = prettier_settings,
+				vue = prettier_settings,
+				css = prettier_settings,
+				scss = prettier_settings,
+				less = prettier_settings,
+				html = prettier_settings,
+				json = prettier_settings,
+				jsonc = prettier_settings,
+				yaml = prettier_settings,
+				markdown = prettier_settings,
+				["markdown.mdx"] = prettier_settings,
+				handlebars = prettier_settings,
+				graphql = prettier_settings,
 				lua = { "stylua" },
+				python = { "isort", "black" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
