451fe7e6f2b62b9539a0c665e559c14266d24adc
Simon Parker <simon.parker1@homegroup.org.uk>
2025-08-17 22:45:31 +0100

    feat: add noice plugin

---
+++ .config/nvim/lua/sjparker94/plugins/noice.lua
@@ -0,0 +1,28 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		views = {
+			cmdline_popup = {
+				position = {
+					row = 5,
+					col = "50%",
+				},
+				size = {
+					width = 60,
+					height = "auto",
+				},
+			},
+		},
+
+		-- add any options here
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- OPTIONAL:
+		--   `nvim-notify` is only needed, if you want to use the notification view.
+		--   If not available, we use `mini` as the fallback
+		-- "rcarriga/nvim-notify",
+	},
+}
